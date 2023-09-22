7cdf9b29a212dfc84344e0ab27b5f312d8276590
leo1806 <cai.leonel.gonzalez@gmail.com>
2023-09-21 21:46:58 -0300

    Preentrega 2

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-const precioSilla = 20000;
-const precioEleva = 4000;
-const precioMuletas = 10000;
-const precioBotaWalker = 5000;
-const precioBaston = 4000;
-
-let carrito = 0;
+const productos = [
+  { nombre: "Silla de ruedas", precio: 20000 },
+  { nombre: "Eleva inodoro", precio: 15000 },
+  { nombre: "Muletas", precio: 10000 },
+  { nombre: "Bota Walker", precio: 5000 },
+  { nombre: "Bastón de madera", precio: 4000 },
+];
 
 function calcularPrecio(producto) {
-  switch (producto.toLowerCase()) {
-    case "silla de ruedas":
-      return precioSilla;
-    case "muletas":
-      return precioMuletas;
-    case "bota walker":
-      return precioBotaWalker;
-    case "baston de madera":
-      return precioBaston;
-    case "eleva inodoro":
-      return precioEleva;
-    default:
-      return 0; 
+  const productoEncontrado = productos.find((el) => el.nombre.toLowerCase() === producto.toLowerCase());
+  if (productoEncontrado) {
+    return productoEncontrado.precio;
+  } else {
+    return 0;
   }
 }
 
+function calcularPrecioConIVA(precio) {
+  const precioConIVA = precio * 1.21;
+  return precioConIVA;
+}
+
+let carrito = [];
+
 while (true) {
-  let producto = prompt("Bienvenido a Ortopedia Belgrano, seleccione el producto que desee comprar: \nSilla de ruedas ($20000) \nEleva inodoro ($15000) \nMuletas ($10000) \nBota Walker ($5000) \nBastón de madera ($4000) \n Para finalizar la compra, escriba 'fin'");
+  let producto = prompt("Bienvenido a Ortopedia Belgrano, seleccione el producto que desee comprar:" + "\n" + 
+  "Silla de ruedas ($20000)" + "\n" + 
+  "Eleva inodoro ($15000)" + "\n" +
+  "Muletas ($10000)" + "\n" + 
+  "Bota Walker ($5000)" + "\n" + 
+  "Bastón de madera ($4000)" + "\n" +
+  "Para finalizar la compra, escriba 'fin'");
 
   if (producto.toLowerCase() === "fin") {
     break;
@@ -33,7 +38,8 @@ while (true) {
   const precioProducto = calcularPrecio(producto);
 
   if (precioProducto > 0) {
-    carrito += precioProducto;
+    const precioConIVA = calcularPrecioConIVA(precioProducto);
+    carrito.push({ nombre: producto, precioSinIVA: precioProducto, precioConIVA: precioConIVA });
     alert(producto + " agregado al carrito.");
   } else {
     alert("Ese producto no es correcto, vuelva a ingresar un producto.");
@@ -41,11 +47,18 @@ while (true) {
 }
 
 function mostrarResumenCompra(carrito) {
-  if (carrito > 0) {
-    alert("El total de su compra es: $" + carrito);
+  if (carrito.length > 0) {
+    const totalSinIVA = carrito.reduce((total, producto) => total + producto.precioSinIVA, 0);
+    const totalConIVA = carrito.reduce((total, producto) => total + producto.precioConIVA, 0);
+    alert("Resumen de la compra:\n" +
+      "Total (sin IVA): $" + totalSinIVA + "\n" +
+      "Total (con IVA): $" + totalConIVA);
   } else {
     alert("No ha agregado ningún producto al carrito.");
   }
 }
 
 mostrarResumenCompra(carrito);
+
+
+
