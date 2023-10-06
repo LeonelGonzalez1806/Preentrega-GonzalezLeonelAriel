f4f68e3ce98ae1631d632f6663d41467c4f831e3
leo1806 <cai.leonel.gonzalez@gmail.com>
2023-10-05 23:21:51 -0300

    preentrega 3

---
+++ turnos.js
@@ -0,0 +1,60 @@
+const formulario = document.getElementById("formulario-turnos");
+const listaTurnos = document.getElementById("lista-turnos");
+
+formulario.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const email = document.getElementById("email").value;
+    const telefono = document.getElementById("telefono").value;
+    const profesional = document.getElementById("profesional").value;
+    const fecha = document.getElementById("fecha").value;
+    const hora = document.getElementById("hora").value;
+
+    const turno = {
+        nombre,
+        apellido,
+        email,
+        telefono,
+        profesional,
+        fecha,
+        hora,
+    };
+
+    // Obtener los turnos almacenados en el almacenamiento local
+    let turnosGuardados = JSON.parse(localStorage.getItem("turnos"));
+
+    // Verificar si los turnos guardados no existen o son nulos
+    if (!turnosGuardados) {
+        turnosGuardados = [];
+    }
+
+    // Guardar el nuevo turno
+    turnosGuardados.push(turno);
+
+    // Almacenar en el almacenamiento local
+    localStorage.setItem("turnos", JSON.stringify(turnosGuardados));
+
+    // Mostrar el turno en la lista
+    const elementoTurno = document.createElement("li");
+    elementoTurno.textContent = `Nombre: ${nombre} - Profesional: ${profesional} - Fecha: ${fecha} - Hora: ${hora}`;
+    listaTurnos.appendChild(elementoTurno);
+
+    // Limpiar el formulario
+    formulario.reset();
+});
+
+// Cargar los turnos guardados en el almacenamiento local al cargar la página
+const turnosGuardados = JSON.parse(localStorage.getItem("turnos"));
+
+// Verificar si los turnos guardados no existen o son nulos
+if (turnosGuardados) {
+    turnosGuardados.forEach(function (turno) {
+        const elementoTurno = document.createElement("li");
+        elementoTurno.textContent = `Nombre: ${turno.nombre} - Profesional: ${turno.profesional} - Fecha: ${turno.fecha} - Hora: ${turno.hora}`;
+        listaTurnos.appendChild(elementoTurno);
+    });
+}
+
+
